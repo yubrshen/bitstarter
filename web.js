57b80dd880a0b11aef3e53938ccedd50b74cb7fa
Ubuntu <ubuntu@ip-172-31-43-229.us-west-2.compute.internal>
2013-07-10 06:55:10 +0000

    Add () after toString in web.js

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 // make the send conten read from index.html
 var fs = require('fs');
 var indexFile = 'index.html';
-var content = fs.readFileSync(indexFile).toString;
+var content = fs.readFileSync(indexFile).toString('utf-8');
+console.log(content)
 
 app.get('/', function(request, response) {
   response.send(content);
